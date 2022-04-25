9fb7de5ea230bcbcb72aff4c52f8055e2a672000
canbaymert <cnbymrt@gmail.com>
2022-04-25 23:45:36 +0300

    string

---
+++ src/_01_Variables/C03_string.java
@@ -0,0 +1,12 @@
+package _01_Variables;
+
+public class C03_string {
+    public static void main(String[] args) {
+        int level=5, age=32, salary=10000;
+        String name,   surname,   birthplace;
+        name="Obi-Wan";
+        surname="Kenobi";
+        birthplace="Stewjon";
+        System.out.print(name+" "+surname+" "+birthplace);
+    }
+}
