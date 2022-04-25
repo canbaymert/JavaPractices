1c7ad6e6472bfa3932133d70300bccf18aba5c69
canbaymert <cnbymrt@gmail.com>
2022-04-26 00:40:23 +0300

    Scanner

---
+++ src/_02_Scanner/C05.java
@@ -0,0 +1,22 @@
+package _02_Scanner;
+
+import java.util.Scanner;
+
+public class C05 {
+    public static void main(String[] args) {
+
+        // Find the hypotenuse of a triangle
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Length of the first leg of right-angled triangle: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Length of the second leg of right-angled triangle: ");
+        int b = scanner.nextInt();
+
+        double h = Math.sqrt((a * a) + (b * b));
+        System.out.println("Length of the Hypotenuse: " + h);
+
+    }
+}
