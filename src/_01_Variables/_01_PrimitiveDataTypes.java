e2773f6db5eb918045fd572c0c6e85967768ec0d
canbaymert <cnbymrt@gmail.com>
2022-04-25 23:30:38 +0300

    variables

---
+++ src/_01_Variables/_01_PrimitiveDataTypes.java
@@ -0,0 +1,14 @@
+package _01_Variables;
+
+public class _01_PrimitiveDataTypes {
+    public static void main(String[] args) {
+        boolean isStudent = false; //true or false
+        char goodmorning = 'g'; //one char
+        byte age = 31; //between -128 and 127 (8 bit)
+        short populationOfVillage = 27324;  //between -32767 and 32767 (16 bits)
+        int populationOfTurkey = 67324564; //between -2,147,483,648 and 2,147,483,647  (32 bits)
+        long accountballance = 1924552512; //between -9,223,372,036,854,775,808 and 9,223,372,036,854,775,807 (64 bits)
+        float averagelevel = 32.123456f;// small floating numbers (64 bits)
+        double averagepoint = 54531231231232141.31241245125421512;
+    }
+}
